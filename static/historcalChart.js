38a4acacab3aac9405a38ff6a5cc04d36360fb1c
Thai Anh <thaianh494@gmail.com>
2025-03-03 22:10:05 +0700

    minimize redundancy

--- static/historcalChart.js
+++ static/historcalChart.js
@@ -1,73 +1,153 @@
-const xml = new XMLHttpRequest();
-xml.open('GET', 'http://127.0.0.1:5000/data/mongo', true);
+function fetchData() {
+    const xml = new XMLHttpRequest();
+    xml.open('GET', 'http://127.0.0.1:5000/data/mongo', true);
 
-xml.responseType = 'json';
-xml.onload = function() {
-    if (xml.status != 200) {
-        alert(`Error ${xml.status}: ${xml.statusText}`);
-    }else {
-        const response = xml.response;
-        //let avg = 0;
-        let lastValue = response[response.length-1]
-        let temp = lastValue['temp'];
-        let humid = lastValue['humid'];
-        
-        //Date to String
-        function takeTime() {
-            let ISODate = new Date(lastValue.date['$date'])
-            let year = ISODate.getFullYear();
-            let month = String(ISODate.getMonth()<8 ? '0' + ISODate.getMonth() : ISODate.getMonth());
-            let day = String(ISODate.getDate()<8 ? '0' + ISODate.getDate() : ISODate.getDate());
-            let time = `${year}-${month}-${day}`;
-            console.log(time);
-            return time;
-        }
-        
-        //Chart
-        var data = {
-                labels: [takeTime()],
-                datasets: [{
-                    label: 'Current Temperature',
-                    data: [temp],
-                    borderWidth: 1
-                }, {
-                    label: 'Current Humidity',
-                    data: [humid],
-                    borderWidth: 1
-                }]}
-        var ctx = document.getElementById('myChart');
-        var chart = new Chart(ctx, {
-        type: 'bar',
-        data: data,
-            options: {
-                responsive: false
-            }
-        });
-        console.log(chart.data.datasets[0].data);
-        //Update the chart
-        function addData() {
-            if (chart) {
-                chart.data.datasets[0].data.push(temp);
-                chart.data.datasets[1].data.push(humid);
-                chart.data.labels.push(takeTime());
+    xml.responseType = 'json';
+    xml.onload = function() {
+        if (xml.status != 200) {
+            alert(`Error ${xml.status}: ${xml.statusText}`);
+        }else {
+            let response = xml.response;
+            let lastValue = response[response.length-1];
+            let latestTempValue = lastValue['temp'];
+            let latestHumidValue = lastValue['humid'];
 
-                //Remove data if exceed 20 docs
-                if (chart.data.labels.length > 5) {
-                    chart.data.labels.shift();
-                    chart.data.datasets.forEach((dataset) => {
-                        dataset.data.shift();
-                    })
+            //Fetch latest 20docs
+            endIndex = response.length
+            startIndex = response.length-21;
+            latestTempList = [];
+            latestHumidList = [];
+            labels = [];
+            if(endIndex > 20) {
+                for(let i = startIndex; i < endIndex; i++) {
+                    latestTempList.push(response[i]['temp']);
+                    latestHumidList.push(response[i]['humid']);
+                    labels.push(ISOParser(response[i].date['$date']));
                 }
-                chart.update();
-            }else {
-                console.warn('Chart instance was null. Skipping Update')
             }
+
+            //ISO1806 String parser
+            function ISOParser(isoString) {
+                let ISODate = new Date(isoString)
+                let year = ISODate.getFullYear();
+                let month = String(ISODate.getMonth()<8 ? '0' + ISODate.getMonth() : ISODate.getMonth());
+                let day = String(ISODate.getDate()<8 ? '0' + ISODate.getDate() : ISODate.getDate());
+                let hour = String(ISODate.getUTCHours()).padStart(2, "0");
+                let minute = String(ISODate.getUTCMinutes()).padStart(2, "0");
+                let second = String(ISODate.getUTCSeconds()).padStart(2, "0");
+                let date = `${year}-${month}-${day}`;
+                let time = `${hour}-${minute}-${second}`; 
+                return time;
+            }
+            
+            // Convert ISO1806 time format to DD-MM-YYYY:hh-mm-ss
+            function takeTime() {
+                let isoString = lastValue.date['$date']
+                let ISODate = new Date(isoString)
+                let year = ISODate.getFullYear();
+                let month = String(ISODate.getMonth()<8 ? '0' + ISODate.getMonth() : ISODate.getMonth());
+                let day = String(ISODate.getDate()<8 ? '0' + ISODate.getDate() : ISODate.getDate());
+                let hour = String(ISODate.getUTCHours()).padStart(2, "0");
+                let minute = String(ISODate.getUTCMinutes()).padStart(2, "0");
+                let second = String(ISODate.getUTCSeconds()).padStart(2, "0");
+                let date = `${year}-${month}-${day}`;
+                let time = `${hour}-${minute}-${second}`; 
+                console.log(lastValue)
+                return time;
+            }
+
+            /*
+            *Chart for visualization
+            */
+            // If chart already exists
+            if (window.tempChart) {
+                // Update data
+                window.tempChart.data.datasets[0].data.push(latestTempValue);
+                window.tempChart.data.labels.push(takeTime());
+
+                // Remove old data if exceeding 20 points
+                if (window.tempChart.data.labels.length > 20) {
+                    // Temp chart
+                    window.tempChart.data.labels.shift();
+                    window.tempChart.data.datasets.forEach(dataset => dataset.data.shift());
+                }
+                window.tempChart.update();
+            } else {
+                // Create Chart if it doesn't exist
+                // Temp chart
+                var chartOne = document.getElementById('myTempChart');
+                window.tempChart = new Chart(chartOne, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [{ 
+                            label: 'Current Temperature', 
+                            data: latestTempList, 
+                            borderWidth: 1,
+                            borderColor: 'rgb(227, 94, 32)',
+                        }]
+                    },
+                    options: { 
+                        responsive: true,
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    // This more specific font property overrides the global property
+                                    font: {
+                                        size: 20
+                                    }
+                                },
+                            }
+                        }
+                    }
+                });
+            }
+
+            if (window.humidChart) {
+                // Update data
+                window.humidChart.data.datasets[0].data.push(latestHumidValue);
+                window.humidChart.data.labels.push(takeTime());
+
+                // Remove old data if exceeding 20 points
+                if (window.humidChart.data.labels.length > 20) {
+                    // Humid chart
+                    window.humidChart.data.labels.shift();
+                    window.humidChart.data.datasets.forEach(dataset => dataset.data.shift());
+                }
+                window.humidChart.update();
+            } else {
+                // Create Chart if it doesn't exist
+                // Humid chart
+                var chartTwo = document.getElementById('myHumidChart');
+                window.humidChart = new Chart(chartTwo, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [{ 
+                            label: 'Current Humidity', 
+                            data: latestHumidList, 
+                            borderWidth: 1,
+                        }]
+                    },
+                    options: { 
+                        responsive: true, 
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    // This more specific font property overrides the global property
+                                    font: {
+                                        size: 20
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+
         }
-
-        let timer = setInterval(() => addData(), 5000);
-
     }
-    
-}
-
-xml.send();        
+    xml.send(); 
+}       
+setInterval(fetchData, 5000);
+fetchData();
---
+++ static/serverToClient.js
@@ -0,0 +1,51 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyCc2zXkyRKfHavz6B_ATJnjFepSu9vY0lY",
+authDomain: "sensor-data-6f9b0.firebaseapp.com",
+databaseURL: "https://sensor-data-6f9b0-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId: "sensor-data-6f9b0",
+storageBucket: "sensor-data-6f9b0.firebasestorage.app",
+messagingSenderId: "187832822335",
+appId: "1:187832822335:web:c3b7f6ecaf996a4f38a68a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+import { getDatabase, ref, get, child, set, update, remove } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js";
+
+// Database deplayment
+const db = getDatabase();
+// Get DOM elements
+let thresholdValue = document.getElementById('mySlider').value;
+let updateButton = document.getElementById('updateButton');
+
+// Get threshold value when open
+get(ref(db, '/threshold/moisture')).then(childData => {
+    if (childData.exists()){
+        thresholdValue = childData.val();
+        console.log("Successfully get data: " + thresholdValue)
+    }
+}).catch(() => {
+    console.log(error);
+}) 
+
+// Update threshold via user input
+function updateData() {
+    thresholdValue = document.getElementById('mySlider').value;
+    update(ref(db, '/threshold'), {
+        moisture: parseInt(thresholdValue)
+    }).then(() => {
+        alert("Threshold adjusted Succesfully!!")
+    }).catch(() => {
+        alert('Cannot adjust the threshold.')
+        console.log(error);
+    })
+}    
+
+window.updateData = updateData;
